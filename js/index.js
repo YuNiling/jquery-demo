733611dee07c5149af22f5202bd018265b8d4432
wyl <1689272480@qq.com>
2018-07-16 17:29:46 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+	$(".has_children").click(function(){
+		$(this).addClass("highlight")
+			   .children("a").show().end()
+			   .siblings().removeClass("highlight")
+			   .children("a").hide();
+	});
+	
+});
